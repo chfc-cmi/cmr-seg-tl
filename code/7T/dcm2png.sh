d2de9cd7dc1b91785b57156805aba4b46b4e1a14
Markus Ankenbrand <markus@ankenbrand.me>
2020-06-16 08:52:39 +0000

    Fix script

--- code/7T/dcm2png.sh
+++ code/7T/dcm2png.sh
@@ -1,6 +1,6 @@
 for j in ../dicom/*
 do
-	ID=$(echo $j | rev | cut -c1,2 | rev)
+	ID=$(echo $j | rev | cut -f1 -d/ | rev)
 	echo $ID
 	# check InPlanePhaseEncodingDirection
 	if [ $(dcmdump $(ls $j/*/*.dcm | head -n1) | grep InPlanePhaseEncodingDirection | grep -c COL) -ne 0 ]
@@ -9,16 +9,14 @@ do
 		for i in $(ls $j/*/*.dcm | sort -V)
 		do
 			out=$(echo $i | perl -ne '/(\d+)(_c)?_gre/;printf "'$ID'_slice%03d",('$SLICE'-$1);/i(\d+).dcm/;printf "_frame%03d-image.png\n",($1-1)')
-			echo dcm2pnm --write-png --use-window 1 $i images/$out
-			cp $i dcms/$(basename $out .png).dcm
+			dcm2pnm --write-png --use-window 1 $i $out
 		done
 	else
 		SLICE=$(ls $j | cut -f1 -d"_" | sort -n | head -n1)
 		for i in $(ls $j/*/*.dcm | sort -V)
 		do
 			out=$(echo $i | perl -ne '/(\d+)(_c)?_gre/;printf "'$ID'_slice%03d",($1-'$SLICE');/i(\d+).dcm/;printf "_frame%03d-image.png\n",($1-1)')
-			echo dcm2pnm --write-png --use-window 1 $i images/$out
-			cp $i dcms/$(basename $out .png).dcm
+			dcm2pnm --write-png --use-window 1 $i $out
 		done
 	fi
 done
