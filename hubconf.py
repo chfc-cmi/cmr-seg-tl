2e66d4275f252cc19e197c4dbdfd03a37440c25e
Markus Ankenbrand <markus@ankenbrand.me>
2020-07-06 13:26:25 +0200

    Move metrics to global

--- hubconf.py
+++ hubconf.py
@@ -3,6 +3,26 @@ import torch
 import tempfile
 from fastai.vision import load_learner
 
+def acc_seg(input, target):
+    target = target.squeeze(1)
+    return (input.argmax(dim=1)==target).float().mean()
+
+def multi_dice(input, targs, class_id=0, inverse=False):
+    n = targs.shape[0]
+    input = input.argmax(dim=1).view(n,-1)
+    # replace all with class_id with 1 all else with 0 to have binary case
+    output = (input == class_id).float()
+    # same for targs
+    targs = (targs.view(n,-1) == class_id).float()
+    if inverse:
+        output = 1 - output
+        targs = 1 - targs
+    intersect = (output * targs).sum(dim=1).float()
+    union = (output+targs).sum(dim=1).float()
+    res = 2. * intersect / union
+    res[torch.isnan(res)] = 1
+    return res.mean()
+
 # cmr_seg_base is the name of entrypoint
 def cmr_seg_base(**kwargs):
     """ # This docstring shows up in hub.help()
@@ -10,26 +30,6 @@ def cmr_seg_base(**kwargs):
     Cardiac Challenge Data: https://www.kaggle.com/c/second-annual-data-science-bowl
     labeled by ukbb_cardiac network https://github.com/baiwenjia/ukbb_cardiac
     """
-    def acc_seg(input, target):
-        target = target.squeeze(1)
-        return (input.argmax(dim=1)==target).float().mean()
-
-    def multi_dice(input, targs, class_id=0, inverse=False):
-        n = targs.shape[0]
-        input = input.argmax(dim=1).view(n,-1)
-        # replace all with class_id with 1 all else with 0 to have binary case
-        output = (input == class_id).float()
-        # same for targs
-        targs = (targs.view(n,-1) == class_id).float()
-        if inverse:
-            output = 1 - output
-            targs = 1 - targs
-        intersect = (output * targs).sum(dim=1).float()
-        union = (output+targs).sum(dim=1).float()
-        res = 2. * intersect / union
-        res[torch.isnan(res)] = 1
-        return res.mean()
-
     # Call the model, load pretrained weights
     url = "https://github.com/chfc-cmi/cmr-seg-tl/releases/download/v0.1.0/resnet34_5percent_size256_extremeTfms_ceLoss.pkl"
     dst = tempfile.NamedTemporaryFile()
