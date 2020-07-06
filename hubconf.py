97ab814ea6c9b482114f4334b3251ea50d0c5de5
Markus Ankenbrand <markus@ankenbrand.me>
2020-07-06 12:28:24 +0200

    Add torch hub config

---
+++ hubconf.py
@@ -0,0 +1,18 @@
+dependencies = ['torch','fastai']
+import torch
+import tempfile
+from fastai.vision import load_learner
+
+# cmr_seg_base is the name of entrypoint
+def cmr_seg_base(**kwargs):
+    """ # This docstring shows up in hub.help()
+    Base model pretrained on the Second Annual Data Science Bowl
+    Cardiac Challenge Data: https://www.kaggle.com/c/second-annual-data-science-bowl
+    labeled by ukbb_cardiac network https://github.com/baiwenjia/ukbb_cardiac
+    """
+    # Call the model, load pretrained weights
+    url = "https://github.com/chfc-cmi/cmr-seg-tl/releases/download/v0.1.0/resnet34_5percent_size256_extremeTfms_ceLoss.pkl"
+    dst = tempfile.NamedTemporaryFile()
+    torch.hub.download_url_to_file(url,dst,progress=False)
+    model = load_learner(path=tempfile.gettempdir(),dst)
+    return model
