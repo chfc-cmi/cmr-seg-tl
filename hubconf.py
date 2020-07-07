16e86e542ebde00edc73ef0cf5e558cea0e738ab
Markus Ankenbrand <markus@ankenbrand.me>
2020-07-07 07:39:19 +0200

    Change import of fastai

--- hubconf.py
+++ hubconf.py
@@ -1,7 +1,7 @@
 dependencies = ['torch','fastai']
 import torch
 import tempfile
-from fastai.vision import load_learner
+import fastai.vision
 
 # cmr_seg_base is the name of entrypoint
 def cmr_seg_base(**kwargs):
@@ -14,5 +14,5 @@ def cmr_seg_base(**kwargs):
     url = "https://github.com/chfc-cmi/cmr-seg-tl/releases/download/v0.1.0/resnet34_5percent_size256_extremeTfms_ceLoss.pkl"
     dst = tempfile.NamedTemporaryFile()
     torch.hub.download_url_to_file(url,dst.name,progress=True)
-    model = load_learner(path=tempfile.gettempdir(),file=dst.name,tfm_y=False)
+    model = fastai.vision.load_learner(path=tempfile.gettempdir(),file=dst.name,tfm_y=False)
     return model
