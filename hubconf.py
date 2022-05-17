929ef0ad663f57911e4b560538ebcb0ceb864ab7
Markus Ankenbrand <markus@ankenbrand.me>
2022-05-17 09:22:58 +0200

    Update hubconf to use fastai2 model

--- hubconf.py
+++ hubconf.py
@@ -1,8 +1,8 @@
 dependencies = ['torch','fastai']
 import torch
-import os.path
+import os
 import tempfile
-import fastai.vision
+from fastai.vision.learner import load_learner
 
 # cmr_seg_base is the name of entrypoint
 def cmr_seg_base(**kwargs):
@@ -12,9 +12,11 @@ def cmr_seg_base(**kwargs):
     labeled by ukbb_cardiac network https://github.com/baiwenjia/ukbb_cardiac
     """
     # Call the model, load pretrained weights
-    url = "https://github.com/chfc-cmi/cmr-seg-tl/releases/download/v0.1.0/resnet34_5percent_size256_extremeTfms_ceLoss.pkl"
-    dst = torch.hub.get_dir() + '/chfc-cmi_cmr-seg-tl_master/' + 'resnet34_5percent_size256_extremeTfms_ceLoss.pkl'
+    url = "https://github.com/chfc-cmi/cmr-seg-tl/releases/download/v0.2.0/resnet34_5percent_size256_extremeTfms_ceLoss_fastai2.pkl"
+    dst_dir = torch.hub.get_dir() + '/chfc-cmi_cmr-seg-tl_master/'
+    os.makedirs(dst_dir, exist_ok=True)
+    dst = dst_dir + 'resnet34_5percent_size256_extremeTfms_ceLoss_fastai2.pkl'
     if not os.path.isfile(dst):
         torch.hub.download_url_to_file(url,dst,progress=True)
-    model = fastai.vision.load_learner(path=tempfile.gettempdir(),file=dst,tfm_y=False)
+    model = load_learner(fname=dst)
     return model
